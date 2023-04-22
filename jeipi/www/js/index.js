69e505368d1a79853cde782229d2318f260ff45d
jooapa <jooa.akonpelto@gmail.com>
2023-04-22 14:47:35 +0300

    ds

---
+++ jeipi/www/js/index.js
@@ -0,0 +1,8 @@
+function showGame(){
+    //append div to games div and add hello world to it
+    var div = document.createElement("div");
+    div.innerHTML = "Hello World";
+    document.getElementById("games").appendChild(div);
+    
+
+}
